99ea1dffd7f546a677781dae977be2d582ff0772
SCaviaF <90842085+SCaviaF@users.noreply.github.com>
2022-10-15 12:39:32 +0200

    Update Milestone3.py
    Comentarios

--- sources/Milestone3.py
+++ sources/Milestone3.py
@@ -8,17 +8,21 @@ from Richardson.ConvergenceCode import Conv
 
 "Run"
 
+#Defino tiempo final, numero divisiones y conodiciones iniciales
 tf = 20
 N = 100
 t = linspace(0, tf, N)
 U0 = array([1, 0, 0, 1])
 
+#Calculo el error
 E = Error(Kepler, t, U0, CN)
-
+#Lo grafico
 plt.plot(t, E[:,0])
 plt.show()
 
-[log_E, log_N] = Conv(Kepler, t, U0, CN)
 
+#Calculo los logaritmos de los ratios de convergencia y numero de subdivisiones
+[log_E, log_N] = Conv(Kepler, t, U0, CN, 8)
+#Lo grafico
 plt.plot(log_N, log_E)
 plt.show()
