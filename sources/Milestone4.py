68bb0114ffd5b391e732c212d2ea3cd27af457d0
SCaviaF <90842085+SCaviaF@users.noreply.github.com>
2022-11-03 22:15:30 +0100

    Add files via upload

---
+++ sources/Milestone4.py
@@ -0,0 +1,35 @@
+from numpy import array, linspace
+import matplotlib.pyplot as plt
+from ODEs.SchemesCode import Euler, RK4, CN, EulerInv, LF
+from Functions.KeplerCode import Kepler
+from ODEs.CauchyCode import Cauchy
+from Richardson.ErrorCode import Error
+from Richardson.ConvergenceCode import Conv
+
+"Run"
+
+#Defino tiempo final, numero divisiones y conodiciones iniciales
+tf = 400
+N = 40000
+t = linspace(0, tf, N)
+U0 = array([1, 0, 0, 1])
+
+
+
+#Calculo el error
+E = Error(Kepler, t, U0, LF)
+#Lo grafico
+plt.plot(t, E[:,0])
+plt.plot(t, E[:,1])
+plt.show()
+
+
+"""
+
+#Calculo los logaritmos de los ratios de convergencia y numero de subdivisiones
+[log_E, log_N] = Conv(Kepler, t, U0, EulerInv, 20)
+#Lo grafico
+plt.plot(log_N, log_E)
+plt.show()
+
+"""
